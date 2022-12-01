0e46003c02a5c80227faf1858b21ade2c0a6c20e
Hakan GÜL <hakanngul@icloud.com>
2022-12-01 23:12:18 +0300

    ..

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = 8000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { response } = require("express");
 
 const app = express();
 
@@ -9,25 +10,86 @@ const newspapers = [
   {
     name: "guardian",
     address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
   },
-{
-    name: 'thetimes',
-    address: 'https://www.thetimes.co.uk/environment/climate-change'
-},
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
 
-{
-    name: 'telegraph',
-    address: 'https://www.telegraph.co.uk/climate-change'
-}
+    base: "",
+  },
+
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "https://www.telegraph.co.uk",
+  },
 ];
 
 const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios
+    .get(newspaper.address)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this)
+          .text()
+          .replace(/\r?\n|\r/g, " ")
+          .trimStart();
+        const url = $(this).attr("href");
+
+        articles.push({
+          title: title,
+          url: newspaper.base + url,
+          source: newspaper.name,
+        });
+      });
+    })
+    .catch((err) => {});
+});
 
 app.get("/", (req, res) => {
   res.json("Welcome to my Climate Change News API");
 });
 
 app.get("/news", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", async (req, res) => {
+  const newsPaperId = req.params.newspaperId;
+
+  const newspaper = newspapers.filter((newsP) => newsP.name == newsPaperId)[0];
+  const newspaperAddress = newspaper.address;
+  const newspaperBase = newspaper.base;
+  const newspaperName = newspaper.name;
+
+  axios.get(newspaperAddress).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const specifArticles = [];
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      specifArticles.push({
+        name: newspaperName,
+        title,
+        url: newspaperBase + url,
+      });
+    });
+  });
+  
+  res.json(newspaperAddress)
+  //res.json(specifArticles);
+});
+
+app.get("/oldNews", (req, res) => {
   axios
     .get("https://www.theguardian.com/environment/climate-crisis")
     .then((response) => {
